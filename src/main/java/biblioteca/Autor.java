68ae66c7ce34465672fee9cc910297511a8b28f8
daryoandrade-art <daryoandrade20@hotmail.com>
2026-07-01 13:49:16 -0300

    feat: cria classe autor

---
+++ src/main/java/biblioteca/Autor.java
@@ -0,0 +1,37 @@
+package main.java.biblioteca;
+
+import java.time.LocalDate;
+
+public class Autor {
+    private static int cont = 0;
+
+    private int id;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Autor(String nome, LocalDate dataNascimento){
+        this.id = ++cont;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
