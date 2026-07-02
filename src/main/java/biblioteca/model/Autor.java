cf37661556211e5eb5c3ee9b9234183f5b5e449d
daryoandrade-art <daryoandrade20@hotmail.com>
2026-07-02 20:05:09 -0300

    refactor: remove contador estatico de Autor, adiciona construtor para reconstrucao via banco

--- src/main/java/biblioteca/model/Autor.java
+++ src/main/java/biblioteca/model/Autor.java
@@ -2,36 +2,35 @@ package biblioteca.model;
 
 import java.time.LocalDate;
 
-public class Autor {
-    private static int cont = 0;
-
+public class Autor{
     private int id;
     private String nome;
     private LocalDate dataNascimento;
 
     public Autor(String nome, LocalDate dataNascimento){
-        this.id = ++cont;
         this.nome = nome;
         this.dataNascimento = dataNascimento;
+    }
+
+    public Autor(int id, String nome, LocalDate dataNascimento){
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public LocalDate getDataNascimento() {
         return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public int getId() {
-        return id;
     }
 }
