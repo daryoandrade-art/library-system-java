8899ccc2a47a488e1b078228fc6b5a790228e01f
daryoandrade-art <daryoandrade20@hotmail.com>
2026-07-02 20:44:41 -0300

    feat: adiciona V2 do flyway para popular tabela autor

---
+++ src/main/resources/db/migration/V2__popula_tabela_autor.sql
@@ -0,0 +1,6 @@
+INSERT INTO autor (nome, data_nascimento) VALUES
+('J.K. Rowling', '1965-07-31'),
+('George R.R. Martin', '1948-09-20'),
+('Machado de Assis', '1839-06-21'),
+('Clarice Lispector', '1920-12-10'),
+('J. R. R. Tolkien', '1892-01-03');
